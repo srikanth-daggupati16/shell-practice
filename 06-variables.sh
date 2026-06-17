6fe68e2799555bd317e7a3f7a6617450285d5c18
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-06-17 07:37:20 -0500

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -5,4 +5,6 @@ echo "Current Timestamp is : $TIMESTAMP"
 
 sleep 10
 
-END_TIMESTAMP=$(date)
+END_TIMESTAMP=$(date +%s)
+TOTAL_TIME=$(($END_TIMESTAMP-$START_TIME))
+echo "Total time taken is : $TOTAL_TIME"
