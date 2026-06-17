0d678bd1d9497e1f3b9ac205886acc3a710d4dbf
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-06-17 06:30:54 -0500

    actions

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hi ramesh, How are you doing?"
+echo "ramesh: Hi suresh, I am doing good. How about you?"
+echo "Suresh: I am doing good too. what are you doing these days?"
+echo "Ramesh: I am learning shell scripting these days. it is very interesting"
