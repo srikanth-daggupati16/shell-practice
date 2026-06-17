9238753994b1630af7fd505190bdb936bc30c7f6
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-06-17 06:56:00 -0500

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+echo "$PERSON1: Hi $PERSON2, How are you doing?"
+echo "$PERSON2: Hi $PERSON1, I am doing good. How about you?"
+echo "$PERSON1: I am doing good too. what are you doing these days?"
+echo "$PERSON2: I am learning shell scripting these days. it is very interesting"
