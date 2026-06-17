cc97a0e40ada100dcb783c08678c372fa1f77a74
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-06-17 09:02:22 -0500

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,9 +4,13 @@ USERID=$(id -u)
 if [$USERID -ne 0]; then
   echo "please enter root user access to run this script"
     exit 1
-else
-  echo "you do not have root user access to run this Script"
-    exit 1
     fi
+
+echo "installinf nginx"
   dnf install nginx -y
+if [$? -ne 0]; then
+  echo "nginx inxtallation failed"
+    exit 1
+else
    echo " nginx is installed successfully"
+   fi
