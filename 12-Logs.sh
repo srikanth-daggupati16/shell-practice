90576e4e8f31968061c1e80acf62d236ab10624f
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-06-18 08:02:39 -0500

    actions

--- 12-Logs.sh
+++ 12-Logs.sh
@@ -2,17 +2,17 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/logs/shell-script/"
 LOGS_FILE="/var/logs/shell-script/$0.Log"
 
-if [$USERID -ne 0]; then
-  echo "please enter root user access to run this script"
+if [ $USERID -ne 0 ]; then
+  echo "please enter root user access to run this script" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-      echo "$2 installation failed"
+      echo "$2 installation failed" | tee -a $LOGS_FILE
         exit 1
     else
-      echo "$2 is installed successfully"
+      echo "$2 is installed successfully" | tee -a $LOGS_FILE
     fi
 }
 
